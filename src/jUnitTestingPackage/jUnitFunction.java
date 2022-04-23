025ebe42de86636d1fafff5ffe2010880296f1d6
fst <fst@DESKTOP-HAOCKQU>
2022-04-23 19:12:18 +0530

    jUnitTesting

---
+++ src/jUnitTestingPackage/jUnitFunction.java
@@ -0,0 +1,11 @@
+package jUnitTestingPackage;
+
+public class jUnitFunction {
+	public int addNumbers(int N1, int N2) {
+		return N1+N2;
+	}
+ 
+	public String addStrings(String s1, String s2) {
+		return s1+s2;
+	}
+}
